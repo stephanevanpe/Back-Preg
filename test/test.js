2f8887aada75a69717e43cc0ebd4761910f20a4c
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-02 09:34:16 +0200

    adding server to test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,16 @@
 var assert = require("assert");
+const { init } = require("../server");
 describe("Array", function() {
+  let server;
+
+  beforeEach(async () => {
+    server = await init();
+  });
+
+  afterEach(async () => {
+    await server.stop();
+  });
+
   describe("#indexOf()", function() {
     it("should return -1 when the value is not present", function() {
       assert.equal([1, 2, 3].indexOf(4), -1);
